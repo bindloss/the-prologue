443c2f05d3089edc4c280e8ded2afd29b5480634
Bindloss <sam.bindloss@hotmail.com>
2018-04-11 09:15:29 +0100

    further updates have been made such as javascript gallery function now fully working

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
-	var slideIndex = 1;
+/*
+
+Landing banner script	
+
+*/
+
+var slideIndex = 1;
 	showDivs(slideIndex);
 
 	function plusDivs(n) {
@@ -24,3 +30,41 @@
 	 x[slideIndex-1].style.display = "block";  
 	 dots[slideIndex-1].className += " white";
 	}
+
+/* 
+
+Image gallery script
+
+*/
+
+var galleryIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(a) {
+  showSlides(galleryIndex += a);
+}
+
+// Thumbnail image controls
+function currentSlide(a) {
+  showSlides(galleryIndex = a);
+}
+
+function showSlides(a) {
+  var g;
+  var slides = document.getElementsByClassName("hidden-gallery");
+  var thumbnail = document.getElementsByClassName("thumbnail");
+  var captionText = document.getElementById("caption");
+  if (a > slides.length) {galleryIndex = 1}
+  if (a < 1) {galleryIndex = slides.length}
+  for (g = 0; g < slides.length; g++) {
+    slides[g].style.display = "none";
+    slides[g].style.display = "none";
+  }
+  for (g = 0; g < thumbnail.length; g++) {
+    thumbnail[g].className = thumbnail[g].className.replace(" active", "");
+  }
+  slides[galleryIndex-1].style.display = "block";
+  thumbnail[galleryIndex-1].className += " active";
+  captionText.innerHTML = thumbnail[galleryIndex-1].alt;
+}
